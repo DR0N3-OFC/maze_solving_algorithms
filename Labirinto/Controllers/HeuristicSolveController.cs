085ddb9e16b53067ca32daa8ef48d3a579f16759
Bruno Raphael Facundo <dronecraftbr10@gmail.com>
2023-04-14 18:28:08 -0300

    Forms, MazeCreation and Searches

---
+++ Labirinto/Controllers/HeuristicSolveController.cs
@@ -0,0 +1,96 @@
+﻿using Labirinto.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Labirinto.Controllers
+{
+    public class HeuristicSolveController
+	{
+        private MazeNode[,] maze;
+        private List<MazeNode> ClosedList;
+        private MyPriorityQueue<MazeNode> OpenList;
+        public int steps = 0;
+
+        public HeuristicSolveController(MazeNode[,] maze)
+        {
+            OpenList = new MyPriorityQueue<MazeNode>();
+            ClosedList = new List<MazeNode>();
+            this.maze = maze;
+        }
+
+        internal MazeNode[,] Maze { set => maze = value; }
+
+        public bool FindPath(ref List<MazeNode> path)
+        {
+            int fx = maze.GetLength(0) - 1;
+            int fy = maze.GetLength(1) - 1;
+
+            MazeNode start = maze[0, 0];
+            MazeNode end = maze[fx, fy];
+
+            start.Custo = 0;
+            start.Heuristica = Heuristica(start, end);
+
+            MazeNode current = start;
+
+            // add start node to Open List
+            OpenList.Enqueue(start, start.F);
+
+            path.Add(start);
+
+            while (!OpenList.IsEmpty() && !ClosedList.Contains(end))
+            {
+                current = OpenList.Dequeue();
+
+                ClosedList.Add(current);
+
+                foreach (MazeNode neighbor in current.Neighbors)
+                {
+                    steps++;
+                    if (!ClosedList.Contains(neighbor) && neighbor != null)
+                    {
+                        bool isFound = false;
+                        foreach (var oLNode in OpenList.UnorderedList)
+                        {
+                            if (oLNode == neighbor) { isFound = true; }
+                        }
+                        if (!isFound && neighbor != null)
+                        {
+                            neighbor.Heuristica = Heuristica(neighbor, end);
+                            neighbor.Custo = neighbor.Bounds.Width + neighbor.Predecessor.Custo;
+                            OpenList.Enqueue(neighbor, neighbor.F);
+                        }
+                    }
+                }
+            }
+
+            // construct path, if end was not closed return null
+            if (!ClosedList.Contains(end))
+            {
+                return false;
+            }
+
+            // if all good, return path
+            MazeNode temp = ClosedList[ClosedList.IndexOf(current)];
+            if (temp == null) return false;
+            do
+            {
+                path.Add(temp);
+                temp = temp.Predecessor;
+            } while (temp != start && temp != null);
+            path.Reverse();
+            return true;
+        }
+
+        private float Heuristica(MazeNode node, MazeNode end)
+        {
+            return Math.Abs(node.Bounds.Location.X - end.Bounds.Location.X) + Math.Abs(node.Bounds.Location.Y - end.Bounds.Location.Y);
+        }
+    }
+}
---
+++ Labirinto/Models/MyPriorityQueue.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirinto.Models
+{
+    public class MyPriorityQueue<T>
+    {
+        private List<(T item, float priority)> heap = new List<(T, float)>();
+        public HashSet<T> UnorderedList = new HashSet<T>();
+
+        public void Enqueue(T item, float priority)
+        {
+            UnorderedList.Add(item);
+            heap.Add((item, priority));
+            int i = heap.Count - 1;
+            while (i > 0)
+            {
+                int parentIndex = (i - 1) / 2;
+                if (heap[parentIndex].priority > heap[i].priority)
+                {
+                    Swap(i, parentIndex);
+                    i = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (heap.Count == 0)
+            {
+                throw new InvalidOperationException("PriorityQueue is empty");
+            }
+            T item = heap[0].item;
+            heap[0] = heap[heap.Count - 1];
+            heap.RemoveAt(heap.Count - 1);
+            int i = 0;
+            while (i < heap.Count)
+            {
+                int leftChildIndex = i * 2 + 1;
+                int rightChildIndex = i * 2 + 2;
+                if (leftChildIndex >= heap.Count)
+                {
+                    break;
+                }
+                int minChildIndex = leftChildIndex;
+                if (rightChildIndex < heap.Count && heap[rightChildIndex].priority < heap[leftChildIndex].priority)
+                {
+                    minChildIndex = rightChildIndex;
+                }
+                if (heap[minChildIndex].priority < heap[i].priority)
+                {
+                    Swap(i, minChildIndex);
+                    i = minChildIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return item;
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (var queueItem in heap)
+            {
+                if (queueItem.item.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public bool IsEmpty()
+        {
+            if (heap.Count == 0)
+                return true;
+            return false;
+        }
+
+        private void Swap(int i, int j)
+        {
+            (T item, float priority) temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+        }
+    }
+}
